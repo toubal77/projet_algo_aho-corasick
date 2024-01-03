49e805fa849f18ba0cd5e99a5b7cafd54e7b1f9e
toubal zine-eddine <toubalzineddine77@gmail.com>
2024-01-03 19:27:31 +0100

    script generateurs

---
+++ script.sh
@@ -0,0 +1,8 @@
+#make
+gcc genere-texte.c -o genere-texte
+gcc genere-mots.c -o genere-mots
+./genere-texte 50000 3 > texte3.txt
+./genere-mots 10 5 25 3 > mots3.txt
+#./ac-matrice mots3.txt texte3.txt > res-ac-matrice
+#./ac-hachage mots3.txt texte3.txt > res-ac-hachage
+#diff res-ac-matrice res-ac-hachage
