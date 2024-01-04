a90f412a4e8a2cf3add7565a45824fa932d9d597
toubal zine-eddine <toubalzineddine77@gmail.com>
2024-01-04 16:41:46 +0100

    add/insertion trie hachage

--- ac-hachage.c
+++ ac-hachage.c
@@ -1,5 +1,5 @@
-
 #include <stdlib.h>
+#include <limits.h>
 #include <stdio.h>
 #include <string.h>
 #include "trie.h"
@@ -86,6 +86,119 @@ Trie createTrie(int maxNode) {
     }
 
     return trie;
+}
+
+// Fonction pour lire un mot depuis un fichier et mettre à jour le buffer
+int motFichier(FILE *file, char **buff, size_t *buffSize) {
+    char *mot = fgets(*buff, (int) *buffSize, file);
+    if (mot == NULL) {
+        return -1;
+    }
+    size_t len = strlen(*buff);
+    char end = (*buff)[len - 1];
+    // Élargissement du buffer si le mot n'est pas complètement lu
+    while (end != '\n') {
+        *buffSize *= 2;
+        mot = realloc(*buff, *buffSize);
+        if (mot == NULL) {
+            return -1;
+        }
+
+        mot = fgets(*buff + len, (int) *buffSize / 2, file);
+        if (mot == NULL) {
+            return *(*buff + len) == '\0' ? 0 : -1;
+        }
+
+        len = strlen(*buff);
+        end = (*buff)[len - 1];
+    }
+
+    // Remplacement du caractère de fin de ligne par '\0'
+    (*buff)[len - 1] = '\0';
+
+    return 0;
+}
+
+
+// Fonction pour ajouter un mot au trie
+void insertWord(Trie trie, unsigned char *mot) {
+    struct _trie *t = trie;
+    int old_state = 0;
+    int curr_state = 0;
+    unsigned char *word = mot;
+
+    // Parcours du mot et mise à jour du trie
+    while (*word != '\0' && curr_state != -1) {
+        old_state = curr_state;
+        curr_state = ProchainEtat(t, curr_state, *word);
+        ++word;
+    }
+
+    // Ajout des transitions manquantes pour le reste du mot
+    if (curr_state == -1) {
+        curr_state = old_state;
+        --word;
+        // Vérification du nombre maximal d'états dans le trie
+        if (strlen((char*)word) > (size_t)(t -> maxNode - t -> nextNode)) {
+            printf("Error: maximum number of nodes exceeded\n");
+            return;
+        }
+        // Ajout des transitions manquantes pour le reste du mot
+        while (*word != '\0') {
+            AjouterTransition(t, curr_state, *word, t -> nextNode);
+            curr_state = t -> nextNode;
+            t -> nextNode++;
+            ++word;
+        }
+    }
+
+    // Incrémentation du nombre d'occurrences finales pour le dernier état
+    t -> finite[curr_state]++;
+}
+
+
+// Fonction utilitaire pour ajouter une transition au trie
+static int AjouterTransition(struct _trie *trie, int startNode,
+        unsigned char letter, int targetNode) {
+    // Allocation de mémoire pour la nouvelle transition
+    struct _list *nouvelleTransition = malloc(sizeof *nouvelleTransition);
+    if (nouvelleTransition == NULL) {
+        return -1;
+    }
+    // Initialisation des champs de la nouvelle transition
+    nouvelleTransition -> startNode = startNode;
+    nouvelleTransition -> targetNode = targetNode;
+    nouvelleTransition -> letter = letter;
+
+    // Calcul de l'index dans le tableau des transitions
+    size_t tailleTransition = getTaille(trie -> maxNode);
+    size_t index = hashFunction(startNode, letter) % tailleTransition;
+
+    // Ajout de la nouvelle transition en tête de liste
+    nouvelleTransition -> next = trie -> transition[index];
+    trie -> transition[index] = nouvelleTransition;
+    return 0;
+}
+
+
+// Fonction utilitaire pour calculer la valeur de hachage d'une transition
+static inline size_t hashFunction(int node, unsigned char letter) {
+    return (size_t)node * ((size_t)UCHAR_MAX + 1) + (size_t)letter;
+}
+
+// Fonction utilitaire pour obtenir le prochain état dans le trie
+static int ProchainEtat(struct _trie *trie, int node, unsigned char letter) {
+    size_t index = hashFunction(node, letter) % getTaille(trie -> maxNode);
+    int next = -1;
+    struct _list *transition = trie -> transition[index];
+    while (transition != NULL && next == -1) {
+        if (transition -> startNode == node && transition -> letter == letter) {
+            next = transition -> targetNode;
+        }
+        transition = transition -> next;
+    }
+
+    return next;
 }
 
 // Fonction qui calcule la taille du tableau 'finite'
